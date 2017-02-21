d490b27272eda026c663e42c435dfb6689d56cb4
Anil Ramnanan <anil@peppersauce.org>
2017-02-21 11:51:05 -0400

    Hello World Basic Code

--- app.js
+++ app.js
@@ -22,40 +22,7 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 // Bots Dialogs
 //=========================================================
-var intents = new builder.IntentDialog();
 
-var sentences = [
-   'Gyul u have meh missing yuh like a blind man pelting mango.',
-   'Eh famaly hear meh na. I wha sort yuh out like a bag a lentil peas.',
-   'Gyul yuh have more form dan a secondary school',
-   'You is de dhal in my dhalpourie, without you, I sada',
-   'Ah hope yuh come wit ah library card cuz ah checkin yuh out',
-   'Yuh fadda does cut cane? How yuh sweet so?',
-   'Is your name wi fi....... Cause I definitely feeling a connection',
-   'My name is Three Plumes, I is the best match for you',
-   'Ay gyul if I was tuh get ah dollar fuh every time ah tink bout yuh ah go only have one cuz u never leaving my mind',
-   'Gyul yuh is d X in me CRIX.  Without you I just CRI',
-   'Gyul, yuh working republic bank or wah?? Cuz you are the one for me',
-];
-
-
-maxSentences = sentences.length;
-
-function getRandomSentence() {
-    var index = Math.floor(Math.random() * (maxSentences - 1));
-    return sentences[index];
-}
-
-bot.dialog('/', intents);
-
-intents.matches(/^pickup/i, [
-    function (session, results) {
-        session.send(getRandomSentence());
-    }
-]);
-
-intents.onDefault([
-    function (session, results) {
-      session.send("Welcome to Trini Pickup lines. You want to pickup a Trini ? Just type 'pickup' ");
-    }
-]);
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+})
